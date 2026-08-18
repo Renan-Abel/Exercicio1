84cb236cdfa3286afe797e9a4f2f86e9b15a35be
Renan-Abel <108385@aluno.uricer.edu.br>
2026-08-17 21:33:46 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicio1/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace Exercicio1
+{
+    public partial class MainPage : ContentPage
+    {
+   
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btCalcular_Clicked(object sender, EventArgs e)
+        {
+            float pontoAx = float.Parse(txPontoAx.Text);
+            float pontoAy = float.Parse(txPontoAy.Text);
+            float pontoBx = float.Parse(txPontoBx.Text);
+            float pontoBy = float.Parse(txPontoBy.Text);
+
+            float distancia = (float)Math.Sqrt(Math.Pow(pontoBx - pontoAx, 2) + Math.Pow(pontoBy - pontoAy, 2));
+
+            txNovoValor.Text = String.Format("{0:f2}", distancia);
+        }
+    }
+}
+
